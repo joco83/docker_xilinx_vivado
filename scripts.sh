4a3c6b6a35079e81017c9c63d4d504984c80d49f
Joachim Schmidt <joachim.schmidt@openmailbox.org>
2015-09-27 21:00:09 +0200

    Mise à jour du fichier script.sh

--- scripts.sh
+++ scripts.sh
@@ -2,10 +2,10 @@
 
 function dock_vivado()
 {
-    run_dockapp -d FT -i joco/xilinx-vivado -f /home/joco/docker_share -c /opt/Xilinx/Vivado/2015.2/bin/vivado
+    run_dockapp -d FT -i joco/xilinx-vivado -f ~/docker_share -c /opt/Xilinx/Vivado/2015.2/bin/vivado
 }
 
 function dock_bash()
 {
-    run_dockapp -d FT -i joco/xilinx-vivado -f /home/joco/docker_share -c /bin/bash
+    run_dockapp -d FT -i joco/xilinx-vivado -f ~/docker_share -c /bin/bash
 }
